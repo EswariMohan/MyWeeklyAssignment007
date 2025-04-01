af52d2d3ee968bae5ec090cfac7964ccfce40edd
EswariMohan <eswaribalraj97@gmail.com>
2025-04-01 17:03:46 +0530

    Add files via upload
    week1day1assignment

---
+++ LearnDatatypes.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		float version=91.0f;
+		System.out.println(version);
+		String developer="google";
+		System.out.println(developer);
+		boolean beta=false;
+		System.out.println(beta);
+		int release_year=2008;
+		System.out.println(release_year);
+		Character shortcutkey='c';
+		System.out.println(shortcutkey);
+		
+		
+	}
+
+}
